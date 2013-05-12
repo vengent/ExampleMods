7f238fbdf73c788c28dcd664b08034440ddc7320
magusapollo <blackmagusapollo@gmail.com>
2013-05-12 01:47:08 -0400

    removed currently unused recipes

--- examplemods_common/net/arasaia/ExampleMods/ExampleModRecipes.java
+++ examplemods_common/net/arasaia/ExampleMods/ExampleModRecipes.java
@@ -14,22 +14,11 @@ public class ExampleModRecipes {
     }
 
     private static void addFurnaceRecipes() {
-        /*
-         * FurnaceRecipes .smelting() .addSmelting( Reference.ORE_ID,
-         * Reference.ORE_COPPER_META_ID, new ItemStack( Reference.INGOT_ID,
-         * Reference.INGOT_COPPER_META_ID, 1), 0.5f);
-         */
         FurnaceRecipes.smelting().addSmelting(
                 Reference.ORE_ID,
                 Reference.ORE_COPPER_META_ID,
                 new ItemStack(Reference.INGOT_ID, 1,
                         Reference.INGOT_COPPER_META_ID), 0.5F);
-
-        /*
-         * FurnaceRecipes .smelting() .addSmelting( Reference.ORE_ID,
-         * Reference.ORE_SILVER_META_ID, new ItemStack( Reference.INGOT_ID,
-         * Reference.INGOT_SILVER_META_ID, 1), 1.0f);
-         */
     }
 
     private static void addBuckets() {
